cd8d9c17b0742db9fa899a2503ff2be552f7e251
ddragon1221 <szn0076@auburn.edu>
2022-10-18 13:06:15 -0500

    Update PerceptualMain.py

--- PerceptualMain.py
+++ PerceptualMain.py
@@ -2,6 +2,11 @@
 #The most important thing is a while loop this should be the foundation of the code and be limited to an int value
 #of 3, this is due to us using a three strike system.
 
+#notes: 
+#interval of time/turns per minute
+#fact you did a turn and quality of turn 
+#how smooth the wheel is 
+
 numOfStrikes = 0
 while(numOfStrikes <= 3):
     #Run a code to take a data subset and input into a formula to check
