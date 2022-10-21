63633dc9be78d5da5ba018e03c5e95611ffae7f9
AidanToomey <act0075@auburn.edu>
2022-10-20 21:44:45 -0500

    Update Tkinter.py

--- Tkinter.py
+++ Tkinter.py
@@ -12,33 +12,49 @@ root = tk.Tk()
 root.title("Perceptual Software")
 root.geometry('1920x1080')
 
-#Add deropdown for debug mode
-menu = Menu (root)
-item = Menu (menu)
-item.add_command(label='Debug')
-menu. add_cascade(label='Debug Menue', menu=item)
-root.config (menu=menu)
-
 #Sperates the canvase into diffrent coloms
 root.columnconfigure(5, weight = 1)
 root.rowconfigure(4, weight = 1)
 
+#Add deropdown for debug mode
+def DropdownMenue():
+    menu = Menu (root)
+    item = Menu (menu)
+    item.add_command(label='Debug')
+    menu. add_cascade(label='Debug Menue', menu=item)
+    root.config (menu=menu)
+
 #Create the verticle line seperator
 tkinter.ttk.Separator(root, orient=VERTICAL).grid(column=1, row=0, rowspan=10, sticky='ns', padx=5, pady=5)
 
-
-#Logo Loading
-logo = Image.open("PerceptualLogoTransparent.png")
-logo = logo.resize((250, 280))
-logo = ImageTk.PhotoImage(logo)
-logo_label = tk.Label(image = logo)
-logo_label.image = logo
-
-
+def LoadLogo():
+    logo = Image.open("PerceptualLogoTransparent.png")
+    logo = logo.resize((250, 250))
+    logo = ImageTk.PhotoImage(logo)
+    logo_label = tk.Label(image = logo)
+    logo_label.image = logo
+    logo_label.grid(column = 0, row = 0)
 
 
-# Add the image in the canvas
-logo_label.grid(column = 0, row = 0)
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+DropdownMenue()
+LoadLogo()
 root.mainloop()
