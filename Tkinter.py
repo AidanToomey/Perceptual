a68b8a0629e611aba7e2de0f3258abf05f001d34
sean0207aub <smb0207@auburn.edu>
2022-11-15 13:04:52 -0600

    -mouse_graph
    got rid of mouse graph

--- Tkinter.py
+++ Tkinter.py
@@ -85,38 +85,6 @@ def RefreshGraph():
     dfN.plot(kind='line', legend=True, ax=ax2, color='r', marker='', fontsize=10)
     ax2.set_title('Year Vs. Unemployment Rate')    
 
-def MouseGraph():
-    control = True
-    count = 0
-    t = 1
-    inc = .1
-    check1, check2, check3 = 0, 1, 2
-    screenY = 540
-    turns = 0
-
-    mouse.moveTo(960, screenY)
-    while control:
-        thing = mouse.position()
-        y.append(thing[0] - 960)
-        x.append(inc * t)
-        if thing[1] > 540 or thing[1] < 540:
-            mouse.moveTo(thing[0], 540)
-        count += 1
-        t += 1
-        if count > 2:
-            if (y[check2] < y[check1] and y[check2] < y[check3]) or (y[check2] == y[check1] and y[check2] < y[check3]):
-                print(f'{y[check2]} is the relative minimum.')
-            if (y[check2] > y[check1] and y[check2] > y[check3]) or (y[check2] == y[check1] and y[check2] > y[check3]):
-                print(f'{y[check2]} is the relative maximum.')
-            check1 += 1
-            check2 += 1
-            check3 += 1
-
-        if count > 100:
-            control = False
-            break
-        time.sleep(inc)
-
 def MouseGraph2():
     control = True
     count = 0
